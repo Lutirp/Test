73ab06dd4c594617f06c09f902dfc60958f3e6fe
Lucas Gomes <lucasluabr1@gmail.com>
2026-07-17 15:50:55 -0300

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -1,16 +1,35 @@
-print ("Seja bem-vindo ao MultiApp")
-nome = input ("Digite seu nome: ")
-genero = input ("Gênero: " )
+print("Seja bem-vindo ao MultiApp")
 
-idade = int(input("Idade: "))
+nome = input("Digite seu nome: ")
+genero = input("Gênero: ").lower()
+idade = input("Idade: ")
 
-if idade < 12:
-    categoria = "criança"
-elif idade < 18:
-    categoria = "adolescente"
-elif idade < 60:
-    categoria = "adulto"
+# Nome
+if nome == "":
+    nome = "não informado"
+elif not nome.replace(" ", "").isalpha():
+    nome = "inválido"
+
+# Gênero
+if genero == "":
+    genero = "não informado"
+elif not genero.replace(" ", "").isalpha():
+    genero = "inválido"
+
+# Idade
+if idade.isdigit():
+    idade = int(idade)
+
+    if idade < 12:
+        categoria = "criança"
+    elif idade < 18:
+        categoria = "adolescente"
+    elif idade < 60:
+        categoria = "adulto"
+    else:
+        categoria = "idoso"
 else:
-    categoria = "idoso"
+    categoria = "desconhecida"
+    idade = "não informada"
 
 print(f"Olá, {nome}! Pelo visto temos um(a) {categoria} de {idade} anos do gênero {genero}.")
